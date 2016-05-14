2b091a24293b56553b00114776812aa22442f77b
Till Ehrengruber <till@ehrengruber.ch>
2016-05-14 09:53:36 +0200

    Fixed small typo

--- src/HttpParser.jl
+++ src/HttpParser.jl
@@ -102,7 +102,7 @@ function ParserSettings(on_message_begin_cb::Ptr{Void},
             * " on_header_field_cb, on_header_value_cb, on_header_value_cb,"
             * " on_headers_complete_cb, on_body_cb, on_message_complete_cb,"
             * " on_chunk_header, on_chunk_complete)", :ParserSettings)
-    dummy_cb = cfunction(() -> 0, HTTP_CB...)
+    dummy_cb = cfunction((parser) -> 0, HTTP_CB...)
     ParserSettings(on_message_begin_cb, on_url_cb, on_status_complete_cb,
       on_header_field_cb, on_header_value_cb, on_headers_complete_cb,
       on_body_cb, on_message_complete_cb, dummy_cb, dummy_cb)
